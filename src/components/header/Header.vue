5a20105876d8ef5086f502a31f86fce579d82c5e
Radek Holešinský <radekholesinsky@Radek-MacBook-Air.local>
2020-07-11 13:10:27 +0200

    pristav_plain_landing

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -3,12 +3,33 @@
         <header>
             <img id ="panorama" class ="panorama" src="@/assets/pristav-panorama2.jpg"/>
         </header>
+
+        <!-- {{userName}} -->
+
+        <!-- <button @click='changeUserName()'>Change UserName</button> -->
     </div>
 </template>
 
 <script>
-export default {
-}
+
+  export default {
+
+    props: {
+      userName: String
+    },
+
+    methods: {
+      changeUserName () {
+        this.$emit('changeUserName')
+      }
+    },
+
+    mounted () {
+      console.log(this.$store.state.user.username);
+    },
+  }
+
+  
 </script>
 
 <style>
---
+++ src/components/store/index.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import Vuex from "vuex";
+ 
+Vue.use(Vuex);
+ 
+export default new Vuex.Store({
+ state: {
+     user: {
+        username: 'mattxy',
+        fullName: 'Matt xyz'
+     }
+ },
+ getters: {},
+ mutations: {},
+ actions: {}
+});
